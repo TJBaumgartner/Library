4872c572eabd8a9ed42216ef5fcfe3c51c061a2b
TJBaumgartner <tnorth6@gmail.com>
2023-04-20 22:28:57 -0700

    submit form button adds book to library array

--- javascript.js
+++ javascript.js
@@ -1,16 +1,43 @@
+const submitButton = document.querySelector('[data-submit]');
+//Book Constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        //console.log(this.title + 'by' + this.author, this.pages, this.read);
-        return this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read;
+}
+//Library Array
+let myLibrary = [
+    {
+        title: "Harry Potter",
+        author: "J.K Rowling",
+        pages: 100,
+        read: false
     }
+];
+submitButton.addEventListener('click', addBookToLibrary);
+function addBookToLibrary(e) {
+    e.preventDefault();
+    let title = document.querySelector('#bookTitle').value;
+    let author = document.querySelector('#bookAuthor').value;
+    let pages = document.querySelector('#bookPages').value;
+    let read = document.querySelector('#bookRead').value;
+    if(document.querySelector('#bookRead').checked == false){
+        read = false;
+    }
+    if(document.querySelector('#bookRead').checked == true){
+        read = true;
+    }
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+    console.log(myLibrary[1]);
+    console.log(myLibrary[2]);
+    resetForm();
 }
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
-//theHobbit.info();
-function displayInfo() {
-    console.log(theHobbit.info());
+function resetForm() {
+    document.querySelector('#bookTitle').value = '';
+    document.querySelector('#bookAuthor').value = '';
+    document.querySelector('#bookPages').value = 0;
+    document.querySelector('#bookRead').checked = false;
 }
-displayInfo();
+
